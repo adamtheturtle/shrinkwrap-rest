5c20f8da726eb32353f0931ebaf5fdf5b4be66da
Adam Dangoor <adamdangoor@gmail.com>
2016-05-05 19:17:44 +0100

    Start of reformatting

--- router.js
+++ router.js
@@ -2,33 +2,33 @@ const HttpHashRouter = require('http-hash-router')
 const concat = require('concat-stream')
 const shrinkwrap = require('./shrinkwrap')
 
-function doError(res, error){
+function doError(res, error) {
   res.statusCode = 500
   res.end(error)
 }
 
-module.exports = function(opts){
+module.exports = function(opts) {
 
   var router = HttpHashRouter()
 
   router.set('/v1/shrinkwrap', {
-    POST:function(req, res, opts, cb){
+    POST: function(req, res, opts, cb) {
       
-      req.pipe(concat(function(data){
+      req.pipe(concat(function(data) {
         data = JSON.parse(data.toString())
 
-        if(!data.github_ssh_key){
+        if (!data.github_ssh_key) {
           return doError(res, 'github_ssh_key property required')
         }
 
-        if(!data.package_file){
+        if (!data.package_file) {
           return doError(res, 'package_file property required')
         }
 
         var githubKey = new Buffer(data.github_ssh_key, 'base64').toString()
         var packageJSON = new Buffer(data.package_file, 'base64').toString()
-        shrinkwrap(githubKey, packageJSON, function(err, data){
-          if(err) return doError(res, err)
+        shrinkwrap(githubKey, packageJSON, function(err, data) {
+          if (err) return doError(res, err)
           res.setHeader('Content-type', 'application/json')
           res.statusCode = 200
           res.end(data)
