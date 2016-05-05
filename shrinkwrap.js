86f53add8d79030d4bb6f58d51970b21c6779fbb
Adam Dangoor <adamdangoor@gmail.com>
2016-05-05 17:25:40 +0100

    Start of exec commands to npm shrinkwrap

--- shrinkwrap.js
+++ shrinkwrap.js
@@ -13,11 +13,78 @@ var async = require('async')
   
 */
 
-module.exports = function(githubKey, packageJSON, done){
+module.exports = function(github_key, package_json_content, done){
 
-  console.log(githubKey)
-  console.log(packageJSON)
+    var timestamp = new Date().getTime()
+    var folderPath = '/tmp/shrinkwrap-build-' + depName + version + timestamp
+    var ssh_config = [
+      'Host github',
+      '    User git',
+      '    HostName github.com',
+      '    IdentityFile ' + folderPath + '/github_key',
+      '    StrictHostKeyChecking no'
+    ].join("\n")
 
+    var shrinkwrap_contents = null
+    series([
+      function(next) {
+        fs.mkdir(folderPath, next)
+      },
+      function(next) {
+        fs.writeFile(folderPath + '/package.json', package_json_content, next)
+      },
+      function(next) {
+        fs.writeFile(folderPath + '/github_key', decoded_ssh_key, next)
+      },
+      function(next) {
+        fs.writeFile('/root/.ssh/config', ssh_config, next)
+      },
+      function(next) {
+          cp.exec('chmod 0600 github_key', {
+          cwd: folderPath
+        }, function(err, data, stderr) {
+            if (err) return next(err)
+            if (stderr) return next(stderr)
+            next()
+        })
+      },
+/*      function(next) {
+          cp.exec('eval `ssh-agent -s`', {
+          cwd: folderPath
+        }, function(err, data, stderr) {
+            console.log('eval error')
+            console.dir(err)
+            console.log(data)
+            console.log(stderr)
+            next(err)
+        })
+      },*/
+      function(next) {
+        cp.exec('npm install --cache .', {
+          cwd: folderPath
+        }, function(err, data, stderr) {
+          if (err) return next(err)
+          if (stderr) return next(stderr)
+          next()
+        })
+      },
+      function(next) {
+        cp.exec('npm shrinkwrap', {
+          cwd: folderPath
+        }, function(err, data, stderr) {
+          if (err) return next(err)
+          if (stderr) return next(stderr)
+          next()
+        })
+      },
+    ], function(err){
+      if (err) console.log("FINAL ERROR " + err)
+      if (err) return cb(err)
+      finished(true)
+    })    
+  } else {
+    finished(false)
+  }
   done()
 
 }
